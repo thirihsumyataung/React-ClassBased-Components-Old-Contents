e6ad61f6096bc42e1595cc9a7a5c7b9b44719710
Thiri Hsu Myat Aung <thirihsumyataung@thiris-mbp.myfiosgateway.com>
2021-06-14 14:01:37 -0400

    state : commit

---
+++ state/src/App.js
@@ -0,0 +1,13 @@
+// state, this.setState = hold/access/change data of component
+// props = read only
+
+import React from "react";
+import Booklist from "./Booklist";
+const App = () => (
+  <section>
+    <h1>This is our Application</h1>
+    <Booklist />
+  </section>
+);
+
+export default App;
